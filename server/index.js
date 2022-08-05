69a5dc547513beda2a1b9717cb9089ea075c70a4
pat <patrickchristywork@gmail.com>
2022-08-05 04:22:13 -0400

    f

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)  
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static('../client/build'))
 
 
 app.get("/api", (req, res) => {
